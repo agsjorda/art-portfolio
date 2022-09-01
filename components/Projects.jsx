2abc4964f9b0e0ea9c269056bebb64dd9eeeb576
Arthur <agsjorda@gmail.com>
2022-09-01 11:41:17 -0400

    added projeccts section

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,18 +1,36 @@
-import Image from 'next/image';
 import React from 'react';
 import thom from '../public/assets/projects/thom.jpg';
+import ProjectItem from './ProjectItem';
 
 const Projects = () => {
   return (
     <div id='project' className='w-full'>
-      <div className='max-w-[1240px] mx-autp px-2 py-16 '></div>
-      <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
-        Projects
-      </p>
-      <h2 className='py-4'>What I've built</h2>
-      <div className='grid md:grid-cols-2 gap-8'>
-        <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-graient-to-r from-[#5651e5] to-[#70dff]'>
-          <Image src={thom} alt='/' />
+      <div className='max-w-[1240px] mx-autp px-2 py-16 '>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+          Projects
+        </p>
+        <h2 className='py-4'>What Ive built</h2>
+        <div className='grid md:grid-cols-2 gap-8'>
+          <ProjectItem
+            title='Clothes Search'
+            backgroundImg={thom}
+            projectUrl='/clothes'
+          />
+          <ProjectItem
+            title='Monsters Rolodex'
+            backgroundImg={thom}
+            projectUrl='/clothes'
+          />
+          <ProjectItem
+            title='Arthur Portfolio'
+            backgroundImg={thom}
+            projectUrl='/clothes'
+          />
+          <ProjectItem
+            title='Arthur Portfolio'
+            backgroundImg={thom}
+            projectUrl='/clothes'
+          />
         </div>
       </div>
     </div>
