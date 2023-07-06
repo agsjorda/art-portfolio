e3cb10a737d5789bb209e89f93b35b9ee3419e89
agsjorda <agsjorda@gmail.com>
2023-07-07 01:04:38 +0800

    added social media links

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,17 +1,42 @@
-import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { AiFillGithub } from "react-icons/ai";
+import { BsInstagram } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
+import { FiLinkedin } from "react-icons/fi";
 
 const SocialMedia = () => {
 	return (
 		<div className="app__social">
 			<div>
-				<BsTwitter />
+				<a href="https://github.com/agsjorda" target="_blank" rel="noreferrer">
+					<AiFillGithub />
+				</a>
 			</div>
 			<div>
-				<BsInstagram />
+				<a
+					href="https://www.instagram.com/sziehro/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<BsInstagram />
+				</a>
 			</div>
 			<div>
-				<FaFacebookF />
+				<a
+					href="https://www.linkedin.com/in/arthur-gerald-jorda-b949483b/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					<FiLinkedin />
+				</a>
+			</div>
+			<div>
+				<a
+					href="https://www.facebook.com/akintofesbook/ "
+					target="_blank"
+					rel="noreferrer"
+				>
+					<FaFacebookF />
+				</a>
 			</div>
 		</div>
 	);
